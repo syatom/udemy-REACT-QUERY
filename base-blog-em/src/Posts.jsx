e70e44e212c04e17daa31bc5cd0ea89bfafbdeaa
Simon Piercy <53854902+syatom@users.noreply.github.com>
2021-10-13 08:54:53 +0100

    Section 11
    Code added up to and including section 11 - Pagination.

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,23 +1,29 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useQuery } from 'react-query'
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data } = useQuery('post', fetchPosts) //  first argument is the name of the query and the second it the request function
-  if (!data) return <div></div>
+  const { data, isError, error, isLoading } = useQuery(['post', currentPage], () => fetchPosts(currentPage), {staleTime: 2000}) //  first argument is the name of the query and the second it the request function
+  if (isLoading) return <h3>Loading...</h3>
+  if (isError) return (
+    <React.Fragment>
+      <h3>Oops, something went wrong!</h3>
+      <p>{error.toString()}</p>
+    </React.Fragment>
+  )
 
   return (
     <>
@@ -33,11 +39,19 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button 
+          disabled={currentPage <=1} 
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue - 1)
+          }}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button 
+          disabled={ currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1)
+          }}>
           Next page
         </button>
       </div>
